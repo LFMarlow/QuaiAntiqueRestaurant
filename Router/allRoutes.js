99949b150e1a0372492a1778fb7ce12bd54ce48d
Marlow <thomas59.lesage@gmail.com>
2024-10-15 18:22:42 +0200

    Ajout Réservations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,6 +8,7 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "/pages/signup.html"),
     new Route("/account", "Mon Compte", "/pages/account.html"),
     new Route("/editPassword", "Changement de Mot de Passe", "/pages/editPassword.html"),
+    new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
