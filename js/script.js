fbc314bc39d4558520a15dc2dda7d1868582eda4
LFMarlow <122919718+LFMarlow@users.noreply.github.com>
2024-10-16 17:30:59 +0200

    Modification script JS

--- js/script.js
+++ js/script.js
@@ -1,40 +1,100 @@
 const tokenCookieName = "accesstoken";
+const RoleCookieName = "role"
+const btnSignOut = document.getElementById("signout_btn");
 
-function setToken(token){
+btnSignOut.addEventListener("click", signout);
+
+function showAndHideElementsForRoles()
+{
+    const userConnected = isConnected();
+    const role = getRoles();
+
+    let allElementToEdit = document.querySelectorAll('[data-show]');
+
+    allElementToEdit.forEach(element =>{
+        switch(element.dataset.show){
+            case 'disconnected' :
+                if(userConnected)
+                {
+                    element.classList.add("d-none")
+                }
+                break;
+            case 'connected' :
+                if(!userConnected)
+                {
+                    element.classList.add("d-none");
+                }
+                break;
+            case 'admin' :
+                if(!userConnected || role != 'admin')
+                {
+                    element.classList.add("d-none");
+                }
+                break;
+            case 'client' :
+                if(!userConnected || role != 'client')
+                {
+                    element.classList.add("d-none");
+                }
+                break;
+        }
+    })
+}
+
+function getRoles()
+{
+    return getCookie(RoleCookieName);
+}
+
+function signout()
+{
+    eraseCookie(tokenCookieName);
+    eraseCookie(RoleCookieName);
+    window.location.reload();
+}
+
+function setToken(token)
+{
     setCookie(tokenCookieName, token, 7);
 }
 
-function getToken(){
+function getToken()
+{
     return getCookie(tokenCookieName);
 }
 
-function setCookie(name,value,days) {
-    var expires = "";
+function setCookie(name,value,days) 
+{
+    let expires = "";
     if (days) {
-        var date = new Date();
+        let date = new Date();
         date.setTime(date.getTime() + (days*24*60*60*1000));
         expires = "; expires=" + date.toUTCString();
     }
     document.cookie = name + "=" + (value || "")  + expires + "; path=/";
 }
 
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+function getCookie(name) 
+{
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for(const element of ca) {
+        let c = element;
+        while (c.startsWith(' ')) c = c.substring(1,c.length);
+        if (c.startsWith(nameEQ)) return c.substring(nameEQ.length,c.length);
     }
     return null;
 }
 
-function eraseCookie(name) {   
+function eraseCookie(name) 
+{   
     document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
 
-function isConnected(){
-    if(getToken() == null || getToken == undefined){
+function isConnected()
+{
+    if(getToken() == null || getToken == undefined)
+    {
         return false;
     }
     else{
